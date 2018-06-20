f46c7830627be354f05078ee8e8288125d4f85d2
lootme <a.kozelski@ya.ru>
2018-06-20 21:28:52 +0300

    titles and nav chain

---
+++ blocks/nav_items/module.js
@@ -0,0 +1,24 @@
+var helper = cms.useModule('helper');
+module.exports = {
+		create: (params, onLogicProcessed) => {
+		
+			var result = {items : {}};
+			
+			var path = cms.getCurrentUrl(),
+				pathItems = path.split('/').filter(function(el) {return el.length != 0}),
+				curPathItem,
+				curPathItemUrl;
+			for(var i in pathItems) {
+				curPathItem = pathItems[i].replace(/-.*/, '');
+				curPathItemUrl = '/' + pathItems.slice(0, parseInt(i)+1).join('/') + '/';
+
+				result.items[i] = {
+					name: curPathItem,
+					url: curPathItemUrl
+				}
+			}
+			
+			onLogicProcessed(result);
+			
+		}
+}
